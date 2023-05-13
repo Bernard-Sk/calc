382af6233cfd5bc716929094ff01ee419f0c9927
Bernard-Sk <bernardkosisochukwu@gmail.com>
2023-05-13 20:45:51 +0100

    Changes2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const icon= document.querySelector(".icon")
 const details = document.querySelector(".details");
 const dicon = document.querySelector(".details .close");
+// const display = document.querySelector(".")
 
 icon.addEventListener("click", () => {
    details.classList.add("active");
